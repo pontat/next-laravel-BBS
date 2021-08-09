a1e68c0a283721287990caf19c391c834e756f32
pontat <ponta.t.dev@gmail.com>
2021-08-10 00:21:52 +0900

    feat:comment api init

---
+++ backend/src/app/Http/Controllers/CommentController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Comment;
+use Illuminate\Http\Request;
+
+class CommentController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(Comment $comment)
+    {
+        //
+    }
+
+    public function update(Request $request, Comment $comment)
+    {
+        //
+    }
+
+    public function destroy(Comment $comment)
+    {
+        //
+    }
+}
