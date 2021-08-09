1201b841648589c60a3c4829a37e6e0b7547d5b6
pontat <ponta.t.dev@gmail.com>
2021-08-09 22:56:32 +0900

    feat:top page markup

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,19 +1,37 @@
-import Head from 'next/head'
+import Link from 'next/link'
+import Pagination from '../components/pagination'
 
 export default function Home() {
   return (
-    <div>
-      <Head>
-        <title>掲示板</title>
-        <meta name="description" content="Next.js×Laravel BBS" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <div className="min-h-screen bg-gray-200">
-        <header className="p-3 bg-white border-b-4 border-indigo-600">
-          <h1 className="text-xl font-bold text-gray-800">掲示板</h1>
-        </header>
-        <main className="container mx-auto p-6"></main>
+    <>
+      <div className="flex">
+        <Link href="/posts">
+          <a className="px-4 py-2 bg-blue-600 hover:bg-blue-500 text-sm font-bold text-white rounded shadow hover:shadow-md cursor-pointer">新規作成</a>
+        </Link>
       </div>
-    </div>
+      <div className="grid gap-4 grid-cols-1 md:grid-cols-2 xl:grid-cols-3 mt-3">
+        {[1, 2, 3].map((post, index) => (
+          <div className="bg-white shadow rounded overflow-hidden" key={index}>
+            <div className="p-4">
+              <h3 className="text-lg font-bold text-gray-900">掲示板のタイトル</h3>
+            </div>
+            <dl>
+              <div className="px-4 py-2 bg-gray-100">
+                <dd className="text-sm text-gray-900">内容が入ります。内容が入ります。内容が入ります。内容が入ります。</dd>
+              </div>
+              <div className="flex justify-between p-4 bg-white">
+                <dt className="text-sm font-medium text-gray-500">2020.12.28</dt>
+                <Link href="/posts/1">
+                  <a className="text-sm text-indigo-600 hover:text-indigo-500">詳細</a>
+                </Link>
+              </div>
+            </dl>
+          </div>
+        ))}
+      </div>
+      <div className="mt-5 flex justify-center">
+        <Pagination></Pagination>
+      </div>
+    </>
   )
 }
