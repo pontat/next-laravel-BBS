5687783e8cfff0af75bca5bf1954d0364b9810d2
pontat <ponta.t.dev@gmail.com>
2021-08-09 22:56:54 +0900

    feat:post new page markup

---
+++ frontend/src/pages/posts.tsx
@@ -0,0 +1,31 @@
+import Link from 'next/link'
+
+export default function Posts() {
+  return (
+    <>
+      <h3 className="text-lg font-bold leading-6 text-gray-900">新規作成</h3>
+      <div className="mt-5 shadow rounded overflow-hidden">
+        <div className="p-4 bg-white space-y-6">
+          <div>
+            <label htmlFor="title" className="text-sm font-bold text-gray-900">
+              タイトル
+            </label>
+            <input type="text" id="title" placeholder="タイトルを入力" className="w-full p-3 bg-white text-sm border border-blueGray-300 rounded" />
+          </div>
+          <div>
+            <label htmlFor="content" className="text-sm font-bold text-gray-900">
+              内容
+            </label>
+            <textarea id="content" rows={5} placeholder="内容を入力" className="w-full p-3 bg-white text-sm border border-blueGray-300 rounded"></textarea>
+          </div>
+        </div>
+        <div className="flex justify-between px-4 py-3 bg-gray-100">
+          <button className="px-4 py-2 bg-blue-600 hover:bg-blue-500 text-sm font-bold text-white rounded shadow hover:shadow-md">投稿する</button>
+          <Link href="/">
+            <a className="px-4 py-2 bg-gray-600 hover:bg-gray-500 text-sm font-bold text-white rounded shadow hover:shadow-md cursor-pointer">戻る</a>
+          </Link>
+        </div>
+      </div>
+    </>
+  )
+}
