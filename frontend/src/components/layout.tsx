7539109a0429ed58e221f56d00338f197ad9c8d7
pontat <ponta.t.dev@gmail.com>
2021-08-12 00:07:13 +0900

    refactor:layout.tsx refactor

--- frontend/src/components/layout.tsx
+++ frontend/src/components/layout.tsx
@@ -4,7 +4,7 @@ type Props = {
   children: React.ReactNode
 }
 
-export const Layout: NextPage<Props> = ({ children }) => {
+const Layout: NextPage<Props> = ({ children }) => {
   return (
     <div className="min-h-screen bg-gray-200">
       <header className="p-3 bg-white border-b-4 border-indigo-600">
