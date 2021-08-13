c926eadd0188ef9ba7f0d46c6ba37392bf872595
pontat <ponta.t.dev@gmail.com>
2021-08-14 01:10:18 +0900

    refactor:PostController refactor

--- backend/src/app/Http/Controllers/PostController.php
+++ backend/src/app/Http/Controllers/PostController.php
@@ -33,13 +33,7 @@ class PostController extends Controller
         $post->content = $request->content;
         $post->save();
 
-        return response()->json([
-            'id' => $post->id,
-            'title' => mb_strimwidth($post->title, 0, 45, '...', 'UTF-8'),
-            'content' => mb_strimwidth($post->content, 0, 300, '...', 'UTF-8'),
-            'created_at' => $post->created_at->format('Y.m.d'),
-            'updated_at' => $post->updated_at->format('Y.m.d'),
-        ]);
+        return response()->json($post);
     }
 
     public function show(int $id): JsonResponse
